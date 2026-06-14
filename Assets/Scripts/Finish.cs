fc58c4e76debde7b0015602d4b37b1317b0c3089
Ponchovy <p.monsieur@icloud.com>
2026-06-14 21:25:19 +0200

    auhh
    reduced coin size, added level transition

---
+++ Assets/Scripts/Finish.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Finish : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+}
